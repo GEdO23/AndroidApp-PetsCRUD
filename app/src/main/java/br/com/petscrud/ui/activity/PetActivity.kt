d094a8b1b4f59eb5299c1355ddd86ad375208769
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-02 19:07:54 -0300

    Removendo variable de teste

--- app/src/main/java/br/com/petscrud/ui/activity/PetActivity.kt
+++ app/src/main/java/br/com/petscrud/ui/activity/PetActivity.kt
@@ -1,52 +1,31 @@
 package br.com.petscrud.ui.activity
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import br.com.petscrud.R
 import br.com.petscrud.databinding.PetLayoutBinding
 import br.com.petscrud.models.Pet
 import com.google.gson.Gson
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import com.google.gson.reflect.TypeToken
 import java.time.LocalDate
-
-const val DB_REFERENCE_PETS = "pets"
-const val RTDB_URL = "https://pet-cp-backend-default-rtdb.firebaseio.com/"
 
 class PetActivity : AppCompatActivity() {
 
     private lateinit var binding: PetLayoutBinding
     private var listaPets: List<Pet> = emptyList()
+    private var petUrl = "https://pet-cp-backend-default-rtdb.firebaseio.com/pets.json"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = PetLayoutBinding.inflate( layoutInflater )
-        val defaultPet = Pet("", "", 0f, LocalDate.now())
-
-        binding.pet = defaultPet
-        defaultPet.peso.toString()
+        binding = PetLayoutBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        val gson = Gson()
 
-
-
-//        val gson = Gson()
-//        val mediaType = "application/json".toMediaTypeOrNull()
-
-
-        /*val database = Firebase.database
-        val myRef = database.getReference(DB_REFERENCE_PETS)
-        myRef.setValue("Hello, World!")*/
-    }
-
-    private fun paraEntidade(): Pet {
-        //TODO: Transformar valores dos campos em uma entidade Pet
-        return Pet(
-            "",
-            "",
-            5.0f,
-            LocalDate.now()
-        )
+        binding.apply {
+            btnGravar.setOnClickListener {  }
+            btnPesquisar.setOnClickListener {  }
+        }
     }
 }
